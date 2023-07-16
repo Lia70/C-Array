54ad41ff22f219aecaaf5c73c870d883211aa45b
Alina Isman <you@example.com>
2023-07-16 13:18:15 +0300

    Решение задачи

---
+++ Task20/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача №20 Напишите программу, которая на вход принимает координаты двух точек
+// и находит расстояние между ними в 2D пространстве
+// A(3,6); B(2,1)->5,09
+// A(7,-5); B(1,-1)->7,21
+Console.Write("X1 = ");
+int X1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Y1 = ");
+int Y1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("X2 = ");
+int X2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Y2 = ");
+int Y2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(Math.Round(Math.Sqrt(Math.Pow(X1 - X2, 2) + Math.Pow(Y1 - Y2, 2)),2));
--- Task21/Program.cs
+++ Task21/Program.cs
@@ -1,14 +1,19 @@
-﻿// Задача №21 Напишите программу, которая на вход принимает координаты двух точек
-// и находит расстояние между ними в 2D пространстве
-// A(3,6); B(2,1)->5,09
-// A(7,-5); B(1,-1)->7,21
+﻿// Задача 21 Напишите программу, которая принимает на вход 
+// координаты двух точек и находит расстояние между ними в 3D пространстве.
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
 Console.Write("X1 = ");
 int X1 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Y1 = ");
 int Y1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Z1 = ");
+int Z1 = Convert.ToInt32(Console.ReadLine());
 Console.Write("X2 = ");
 int X2 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Y2 = ");
 int Y2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Z2 = ");
+int Z2 = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine(Math.Round(Math.Sqrt(Math.Pow(X1 - X2, 2) + Math.Pow(Y1 - Y2, 2)),2));
+Console.WriteLine(Math.Round(Math.Sqrt(Math.Pow(X1 - X2, 2) + Math.Pow(Y1 - Y2, 2) + Math.Pow(Z1 - Z2, 2)),2));
